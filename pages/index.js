a9e976faf717ab2eaa835a35a61166b17cc48bc1
Angelica Contreras <afernandac94@gmail.com>
2023-01-26 20:32:41 -0300

    Consultando la base de datos con getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,19 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { PrismaClient } from '@prisma/client';
 
-export default function Home() {
+export default function Home({ categorias }) {
+	console.log('🚀  categorias', categorias);
+
 	return <h1>Next.js</h1>;
 }
 
 export const getServerSideProps = async () => {
 	const prisma = new PrismaClient();
 
-	const categorias = await prisma.categoria.findMany();
-	console.log(categorias);
+	const categorias = await prisma.categoria.findMany({});
+	return {
+		props: {
+			categorias,
+		},
+	};
 };
