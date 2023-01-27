aa7c89e7c54854cf3de652ef678fef4517a5c40a
Angelica Contreras <afernandac94@gmail.com>
2023-01-27 16:33:27 -0300

    Obtener las categorias de la API

---
+++ components/Categoria.js
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+
+const Categoria = ({ categoria }) => {
+	const { nombre, id, icono } = categoria;
+
+	return (
+		<div className="flex ites-center gap-4 w-full border p-5 hover:bg-amber-400">
+			<Image
+				width={70}
+				height={70}
+				src={`/assets/img/icono_${icono}.svg`}
+				alt="Imagen Icono"
+				className="mr-5"
+			/>
+			<button type="button" className="text-2xl font-bold hover:cursor-pointer">
+				{nombre}
+			</button>
+		</div>
+	);
+};
+
+export default Categoria;
--- context/QuiscoProvider.jsx
+++ context/QuiscoProvider.jsx
@@ -1,9 +1,25 @@
 import { useState, useEffect, createContext } from 'react';
+import axios from 'axios';
 
 const QuiscoContext = createContext();
 
 const QuioscoProvider = ({ children }) => {
-	return <QuiscoContext.Provider value={{}}>{children}</QuiscoContext.Provider>;
+	const [categorias, setCategorias] = useState([]);
+
+	const obtenerCategorias = async () => {
+		const { data } = await axios('/api/categorias');
+		setCategorias(data);
+	};
+
+	useEffect(() => {
+		obtenerCategorias();
+	}, []);
+
+	return (
+		<QuiscoContext.Provider value={{ categorias }}>
+			{children}
+		</QuiscoContext.Provider>
+	);
 };
 
 export { QuioscoProvider };
