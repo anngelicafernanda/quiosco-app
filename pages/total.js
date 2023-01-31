50e918d8d5bb776722d3f38c4149a1074ea51c3a
Angelica Contreras <afernandac94@gmail.com>
2023-01-31 13:59:08 -0300

    Trabajando con la pagina de totales

--- pages/total.js
+++ pages/total.js
@@ -5,6 +5,32 @@ export default function Total() {
 		<Layout pagina="Total y Confirmar Pedido">
 			<h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
 			<p className="text-2xl my-10">Confirma tu pedido a Continuación</p>
+			<form action="">
+				<div>
+					<label
+						htmlFor="nombre"
+						className="block uppercase text-slate-800 font-bold text-xl"
+					>
+						Nombre
+					</label>
+					<input
+						id="nombre"
+						type="text"
+						className="bg-gray-200 w-full mt-3 lg:w-1/3 p-2 rounded-md"
+					/>
+				</div>
+				<div className="mt-10">
+					<p className="text-2xl">
+						Total a pagar: {``} <span className="font-bold">$200</span>
+					</p>
+				</div>
+				<div className="mt-5">
+					<input
+						value="Confirmar Pedido"
+						className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-center text-white"
+					/>
+				</div>
+			</form>
 		</Layout>
 	);
 }
