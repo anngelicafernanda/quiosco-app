ddb6175e6cde6d7dd970826d1e4646753048f322
Angelica Contreras <afernandac94@gmail.com>
2023-01-31 14:30:11 -0300

    Desabilitando el boton cuando no se han registrado pedidos

--- pages/total.js
+++ pages/total.js
@@ -1,11 +1,28 @@
+import { useEffect, useCallback } from 'react';
+import useQuiosco from 'hooks/useQuiosco';
 import Layout from 'layout/Layout';
 
 export default function Total() {
+	const { pedido } = useQuiosco();
+
+	const comprobarPedido = useCallback(() => {
+		return pedido.length === 0;
+	}, [pedido]);
+
+	useEffect(() => {
+		comprobarPedido();
+	}, [pedido, comprobarPedido]);
+
+	const colocarOrden = (e) => {
+		e.preventDefault();
+		console.log('Enviando orden');
+	};
+
 	return (
 		<Layout pagina="Total y Confirmar Pedido">
 			<h1 className="text-4xl font-black">Total y Confirmar Pedido</h1>
 			<p className="text-2xl my-10">Confirma tu pedido a Continuación</p>
-			<form action="">
+			<form onSubmit={colocarOrden}>
 				<div>
 					<label
 						htmlFor="nombre"
@@ -26,8 +43,14 @@ export default function Total() {
 				</div>
 				<div className="mt-5">
 					<input
+						type="submit"
 						value="Confirmar Pedido"
-						className="bg-indigo-600 w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-center text-white"
+						className={` ${
+							comprobarPedido()
+								? 'bg-indigo-100 hover:bg-indigo-800'
+								: 'bg-indigo-600'
+						} w-full lg:w-auto px-5 py-2 rounded uppercase font-bold text-center text-white`}
+						disabled={comprobarPedido()}
 					/>
 				</div>
 			</form>
