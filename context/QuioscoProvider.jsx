751776de90b2a1fbef7290c452b86d94313b91d3
Angelica Contreras <afernandac94@gmail.com>
2023-01-31 12:53:27 -0300

    Detectando la url para recordar el progreso

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -10,7 +10,6 @@ const QuioscoProvider = ({ children }) => {
 	const [producto, setProducto] = useState({});
 	const [modal, setModal] = useState(false);
 	const [pedido, setPedido] = useState([]);
-	const [paso, setPaso] = useState(1);
 
 	const obtenerCategorias = async () => {
 		const { data } = await axios('/api/categorias');
@@ -53,10 +52,6 @@ const QuioscoProvider = ({ children }) => {
 		setModal(false);
 	};
 
-	const handleChangePaso = (paso) => {
-		setPaso(paso);
-	};
-
 	return (
 		<QuiscoContext.Provider
 			value={{
@@ -69,8 +64,6 @@ const QuioscoProvider = ({ children }) => {
 				handleChangeModal,
 				handleAgregarPedido,
 				pedido,
-				paso,
-				handleChangePaso,
 			}}
 		>
 			{children}
