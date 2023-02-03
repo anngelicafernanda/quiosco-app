5054e9cc0e2cd947a2fc00ce49af90d6e5946420
Angelica Contreras <afernandac94@gmail.com>
2023-02-03 13:19:45 -0300

    Consultando el Endpoint de Ordenes con useSWR

--- pages/admin.js
+++ pages/admin.js
@@ -1,6 +1,13 @@
+import useSWR from 'swr';
 import AdminLayout from 'layout/AdminLayout';
+import axios from 'axios';
 
 export default function admin() {
+	const fetcher = () => axios('/api/ordenes').then((datos) => datos.data);
+	const { data, error, isLoading } = useSWR('/api/ordenes', fetcher);
+	console.log('🚀  data', data);
+	console.log('🚀  error', error);
+	console.log('🚀  isLoading', isLoading);
 	return (
 		<AdminLayout pagina={'Admin'}>
 			<h1 className="text-4xl font-black">panel de Administracion</h1>
