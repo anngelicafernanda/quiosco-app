eb86ea3f18d32cd8bc50a7cd20c63f9556430f1b
Angelica Contreras <afernandac94@gmail.com>
2023-02-08 13:37:28 -0300

    Añadir tiempo real con useSWR

--- pages/admin.js
+++ pages/admin.js
@@ -5,7 +5,9 @@ import Orden from '../components/Orden';
 
 export default function admin() {
 	const fetcher = () => axios('/api/ordenes').then((datos) => datos.data);
-	const { data, error, isLoading } = useSWR('/api/ordenes', fetcher);
+	const { data, error, isLoading } = useSWR('/api/ordenes', fetcher, {
+		refreshInterval: 100,
+	});
 	// console.log('🚀  data', data);
 	// console.log('🚀  error', error);
 	// console.log('🚀  isLoading', isLoading);
